cfeca1a027d4479b8019e656cbb0502420a6725b
zhangwei <zhangweihust@gmail.com>
2013-11-21 11:11:50 +0800

    fix UI

--- s_kline_client/src/com/zhangwei/stock/gui/CtrlPanel.java
+++ s_kline_client/src/com/zhangwei/stock/gui/CtrlPanel.java
@@ -7,9 +7,16 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CtrlPanel extends JPanel {
+	JLabel jl;
+	JButton stockPreBtn;
+	JButton stockNxtBtn;
+	JButton tradePreBtn;
+	JButton tradeNxtBtn;
 
 	/**
 	 * 
@@ -19,13 +26,31 @@ public class CtrlPanel extends JPanel {
 	public CtrlPanel(){
 		setLayout(new GridBagLayout());
         
-		ShowPanel showPanel = new ShowPanel();
-        ButtonPanel btnPanel = new ButtonPanel();
+/*		ShowPanel showPanel = new ShowPanel();
+        ButtonPanel btnPanel = new ButtonPanel();*/
 		
-		addComponent(this, showPanel, 0, 0, 1, 0,
+/*		addComponent(this, showPanel, 0, 0, 1, 0,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH);
 		addComponent(this, btnPanel, 1, 0, 1, 0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH);*/
+		
+		jl = new JLabel("<html><HTML><body style=color:red>   date: 2010-10-10 <br> open: 123.33, high: 123.33<br> close: 123.33, low: 123.33<br> vol: 1233390, cje: 1233390<br></body></html>");
+		stockPreBtn = new JButton("<<");
+		stockNxtBtn = new JButton(">>");
+		tradePreBtn = new JButton("<");
+		tradeNxtBtn = new JButton(">");
+		
+		addComponent(this, jl, 0, 0, 2, 2,
                 GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+		addComponent(this, stockPreBtn, 2, 0, 1, 1,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+		addComponent(this, stockNxtBtn, 3, 0, 1, 1,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+		addComponent(this, tradePreBtn, 2, 1, 1, 1,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+		addComponent(this, tradeNxtBtn, 3, 1, 1, 1,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+		
 		
 		//setSize(420, 50);
 		Dimension size = new Dimension(420, 50);
